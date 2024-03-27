d05e4ac4a9c17d6e834f94b7f75a4d9074128e18
Nelson Mecha <nelsonorinamecha@gmail.com>
2024-03-27 21:41:53 +0300

    DOM lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+
+const newHeader = document.createElement('h1')
+newHeader.textContent = 'Javascript Application is the champion'
+newHeader.setAttribute('id', 'victory')
+document.body.append
+console.log(newHeader)
+
